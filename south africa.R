af620ce489eb074af9ebe52eddae97e2c2c30af5
Conor Kelly <conor.richard.kelly@gmail.com>
2021-12-12 11:46:33 -0500

    updates 12/12/2021

--- south africa.R
+++ south africa.R
@@ -9,10 +9,7 @@ library(RColorBrewer)
 
 #setwd("/Users/conorkelly/Documents/COVID")
 
-add_row <- FALSE
-cases_temp <- 22391
-deaths_temp <- 22
-date_temp <- Sys.Date()
+
 
 omcr_annc_date <- ymd("2021-11-25")
 
@@ -25,26 +22,25 @@ afr <- raw %>%
 s_afr <- raw %>%
   filter(location %in% "South Africa") %>%
   select(date, new_cases, new_cases_smoothed, new_deaths, new_deaths_smoothed,
-         positive_rate) %>%
+         positive_rate, new_tests_smoothed) %>%
   mutate(new_cases = ifelse(is.na(new_cases), 0 , new_cases),
          temp = 0)
 
 if(add_row == TRUE){
-s_afr <- s_afr%>%
+s_afr <- s_afr %>%
   add_row(new_cases = cases_temp,
           new_deaths = deaths_temp,
           date = date_temp,
-          temp = 1) %>%
-  mutate(new_cases_smoothed = ifelse(is.na(new_cases_smoothed),
-                                     movavg(new_cases, 7),
-                                     new_cases_smoothed)) %>%
-  mutate(new_deaths_smoothed = ifelse(is.na(new_deaths_smoothed),
-                                     movavg(new_deaths, 7),
-                                     new_deaths_smoothed)) %>%
+          temp = override) %>%
   group_by(date) %>%
   filter(temp == min(temp, na.rm = TRUE)) %>%
   ungroup()
+  
 }
+
+s_afr <- s_afr %>%
+  mutate(new_cases_smoothed = movavg(new_cases, 7),
+         new_deaths_smoothed = movavg(new_deaths, 7))
 
 # lags
 case_peak_date <- ymd("2021-07-08")
@@ -105,20 +101,37 @@ cfr_plot <- s_afr %>%
 cfr_plot
 
 # pos rate
-pos_rate_plot <- s_afr %>%
+
+# assign ratios for axis transformation
+s_afr$positive_rate_int = s_afr$positive_rate * 100
+max_tests <- max(s_afr$new_tests_smoothed, na.rm = TRUE)
+max_pp <- max(s_afr$positive_rate_int, na.rm = TRUE)
+
+pp_test_ratio <- max_tests / max_pp
+
+s_afr_pp <- s_afr %>%
   filter(date >= "2021-01-01") %>%
+  mutate(tests_axis2 = new_tests_smoothed / pp_test_ratio / 100)
+
+pos_rate_plot <- s_afr_pp %>%
+
   ggplot +
   geom_point(aes(x = date, y = positive_rate), 
              color = "forestgreen", size = 1, shape = 1, alpha = 0.75) +
   geom_line(aes(x = date, y = positive_rate), 
              color = "forestgreen", size = 0.2, alpha = 0.5) +
-  scale_y_continuous(labels = percent_format(accuracy = 2))+
+  geom_area(aes(x = date, y = tests_axis2, label = new_tests_smoothed), 
+            fill = "forestgreen", alpha = 0.15) +
+  scale_y_continuous(labels = percent_format(accuracy = 2),
+                     sec.axis = sec_axis(~.*pp_test_ratio, name = "Daily Tests (7-Day Avg)")) +
   theme_minimal() +
-  labs(title = "South Africa: Positive Rate") +
+  labs(title = "Positivity Rate (Lines) and 7-Day Avg Daily Testing (Area)") +
   xlab("") +
-  ylab("") +
+  ylab("Positive Rate (7-Day)") +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+pos_rate_plot
 
 # ratios
 sa_ratio <- s_afr %>%
@@ -164,17 +177,19 @@ sa_ratio_plot <-
   filter(date >= "2021-01-01") %>%
   ggplot() +
   geom_area(aes(x = date, y = abv_one), fill = '#f2947f', alpha = 0.2) +
-  geom_area(aes(x = date, y = one), fill = '#99e5e7', alpha = 0.5) +
+  geom_area(aes(x = date, y = one), fill = '#99e5e7', alpha = 0.5) + 
   geom_point(aes(x = date, y = case_ratio, color = case_decline), 
              alpha = 0.5, size = 0.7, show.legend = FALSE) +
-  geom_line(aes(x = date, y = case_ratio_smoothed)) +
+  geom_line(aes(x = date, y = case_ratio_smoothed), size = 0.8) +
+  geom_hline(yintercept = 1, linetype = 'dashed', size = 0.7, alpha = 0.6) +
   theme(legend.position='none') +
   ggtitle("Growth Multiplier of COVID-19 Cases Week-Over-Week") +
   xlab("") +
   ylab("") +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
-  scale_y_continuous(breaks = c(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12),
-                     labels = percent_format(accuracy = 2)) +
+  scale_y_continuous(breaks = c(0, 0.01, 0.1, 0.5, 0.75, 1, 1.5, 2, 3, 4, 5, 6, 7, 8),
+                     #labels = percent_format(accuracy = 1),
+                     trans = "log2") +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
 
@@ -339,7 +354,7 @@ pct_hosp_plot <- comb %>%
          pct_val = ifelse(variable %in% "Cases", NA, pct_val)) %>%
   
   ggplot() +
-  geom_area(aes(x = Date, y = area), fill = 'coral1', alpha = 0.4) +
+  geom_area(aes(x = Date, y = area), fill = 'lightgray', alpha = 0.7) +
   geom_line(aes(x = Date, y = pct_val, color = variable), 
             size = 0.8) +
   geom_vline(xintercept = as.numeric(omcr_annc_date), linetype = "dashed") +
@@ -353,10 +368,10 @@ pct_hosp_plot <- comb %>%
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
   labs(variable = "" ) +
-  scale_color_manual(values = c("Cases" = "coral1",
-                                "Total Admissions" = "azure4",
-                                "ICU" = "steelblue1",
-                                "Ventilated" = "steelblue4"))
+  scale_color_manual(values = c("Cases" = "lightgray",
+                                "Total Admissions" = "#6F8FAF", # denim blue
+                                "ICU" = "#6495ED", # cornflower (medium)
+                                "Ventilated" = "#00008B")) # dark blue
   
 pct_hosp_plot
 ggplotly(pct_hosp_plot)
@@ -374,14 +389,14 @@ icu_pct_plot <- icu %>%
   filter(variable %in% c("CurrentlyVentilated", "CurrentlyinICU")) %>%
   mutate(Province = factor(Province, levels = c("Total", "Gauteng"))) %>%
   ggplot() +
-  geom_line(aes(x = Date, y = pct_of_admitted, color = Province)) +
+  geom_line(aes(x = Date, y = pct_of_admitted, color = variable), size = 0.8) +
   geom_vline(xintercept = as.numeric(omcr_annc_date), linetype = "dashed") +
-  facet_wrap(~variable) +
+  facet_wrap(~Province) +
   
   ylim(0, .3) +
   scale_y_continuous(labels = percent_format(accuracy = 1),
                      limits = c(0, .3),
-                     breaks = seq(0, .3, 0.02)) +
+                     breaks = seq(0, .3, 0.05)) +
   ylab("") +
   xlab("") +
   
@@ -389,10 +404,87 @@ icu_pct_plot <- icu %>%
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
   scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y") +
-  scale_color_brewer(palette = "Dark2") +
-  labs(variable = "" )
-
+  labs(variable = "" ) +
+  scale_color_manual(values = c("CurrentlyinICU" = "#6495ED",
+                                "CurrentlyVentilated" = "#00008B")) +
+  theme(strip.text.x = element_text(size = 11, face = 'bold'))
+icu_pct_plot
 ggplotly(icu_pct_plot)
+
+##########################################
+## gauteng
+gauteng <- read_csv("/Users/conorkelly/Documents/COVID/gauteng_cases.csv") %>%
+  mutate(new_cases_smoothed = movavg(new_cases, 7),
+         ratio_smooth = new_cases_smoothed / lag(new_cases_smoothed, 7),
+         ratio = new_cases / lag(new_cases, 7),
+         date = mdy(date))
+
+### Gauteng cases base
+gt_cases_plot <- 
+  gauteng %>%
+  filter(date >= "2021-01-01") %>%
+  ggplot() +
+  geom_col(aes(x = date, y = new_cases), 
+             fill = "coral1", alpha = 0.4, size = 1) +
+  geom_line(aes(x = date, y = new_cases_smoothed), 
+            color = "coral1", alpha = 0.5, size = 0.8) +
+  xlab("") +
+  ylab("") +
+  scale_x_date(date_breaks = "1 week") +
+  labs(title = "COVID-19 Cases in Gauteng") +
+  scale_y_continuous(label = comma) +
+  
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1))
+
+## Gauteng log
+gt_log_cases <- 
+  gauteng %>%
+  filter(date >= "2021-01-01") %>%
+  ggplot() +
+  geom_point(aes(x = date, y = new_cases_smoothed), 
+           color = "coral1", shape = 1, alpha = 0.75, size = 1) +
+  geom_line(aes(x = date, y = new_cases_smoothed), 
+            color = "coral1", alpha = 0.5, size = 0.2) +
+  xlab("") +
+  ylab("") +
+  scale_x_date(date_breaks = "1 week") +
+  labs(title = "COVID-19 Cases in Gauteng (Log Scale)") +
+  scale_y_continuous(label = comma) +
+  
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  scale_y_continuous(trans='log2',
+                     label = comma,
+                     breaks=c(0, 25,  50, 100, 250, 500, 1000, 2000, 5000, 10000, 20000, 40000))
+
+## Gauteng ratio
+gt_ratio <-
+  gauteng %>%
+  mutate(one = 1,
+         abv_one = 8) %>%
+  filter(date >= "2021-01-01") %>%
+  ggplot() +
+  geom_area(aes(x = date, y = abv_one), fill = '#f2947f', alpha = 0.2) +
+  geom_area(aes(x = date, y = one), fill = '#99e5e7', alpha = 0.5) + 
+  geom_point(aes(x = date, y = ratio), 
+             color = "black", shape = 1, alpha = 0.75, size = 1) +
+  geom_line(aes(x = date, y = ratio_smooth), 
+            color = "black", alpha = 0.9, size = 0.75) +
+  xlab("") +
+  ylab("") +
+  scale_x_date(date_breaks = "1 week") +
+  scale_y_continuous(breaks = c(0, 0.5, 1, 1.5, 2, 3, 4, 5, 6, 7, 8),
+                     labels = percent_format(accuracy = 1)) +
+  
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(title = "Gauteng Growth Multiplier")
+
+# show
+ggplotly(gt_cases_plot)
+ggplotly(gt_log_cases)
+ggplotly(gt_ratio)
 
            
            
